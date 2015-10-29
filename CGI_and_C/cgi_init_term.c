2b5f9d3d24004e69297d3064ca58fbfd648fd969
John Carlo Roberto <john.carlo9517@gmail.com>
2015-10-30 02:14:08 +0800

    added map_screen html test file

--- CGI_and_C/cgi_init_term.c
+++ CGI_and_C/cgi_init_term.c
@@ -11,6 +11,23 @@ void cgi_init(char *title)
   printf("</head><body>");
 }
 
+void cgi_init_beforecss(char *title)
+{
+  printf("Content-type:text/html\n\n");
+  printf("<html><head>");
+  printf("<meta charset=\"UTF-8\">");
+  printf("<title>%s</title>",title);
+  printf("<link rel=\"stylesheet\" type=\"text/css\" href=\"/MP1/bootstrap-3.3.5-dist/css/bootstrap.min.css\">");
+  printf("<link rel=\"stylesheet\" type=\"text/css\" href=\"/MP1/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css\">");
+  printf("<style>");
+}
+
+void cgi_init_aftercss()
+{
+  printf("</style>");
+  printf("</head><body>");
+}
+
 void cgi_term()
 {
   printf("<script src=\"/MP1/bootstrap-3.3.5-dist/js/jquery.min.js\"></script>");
---
+++ CGI_and_C/map_handler.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mysql/mysql.h> //CHANGE THIS TO PROPER DIRECTORY
+#include "cgicustom.h"
+#define STATEMENT_LEN 100
+#define db_name "db_RPG"
+#define USER "root"
+#define PASS "root"
+
+int main()
+{
+	
+}
